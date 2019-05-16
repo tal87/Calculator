33f4c8cd911cf082b65a2a3302dc31db506bbf60
tal87 <talbsho@gmail.com>
2019-05-16 07:51:25 +0300

    Updates

---
+++ precache-manifest.293dfcc6e6c5acaa6ce7a35b4895f07e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e3bb98930e4d9686503696032d478a",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "21eb1d903de2526bb48f",
+    "url": "/Calculator/static/css/main.4f985367.chunk.css"
+  },
+  {
+    "revision": "122081189bb4811381b7",
+    "url": "/Calculator/static/js/2.caf1d88b.chunk.js"
+  },
+  {
+    "revision": "21eb1d903de2526bb48f",
+    "url": "/Calculator/static/js/main.5809dd8d.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  }
+]);
