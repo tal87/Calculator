93102a1652241a680598eb11891f6b599da4191d
tal87 <talbsho@gmail.com>
2019-05-18 10:44:27 +0300

    display chosen operation

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -4,11 +4,27 @@ import { connect } from "react-redux";
 import "./Display.css";
 
 const Display = props => {
-  return <div className="display">{props.result}</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#5b5b5b",
+        border: "1px solid black",
+        fontSize: "20px"
+      }}
+    >
+      <div className="operator">{props.operator}</div>
+      <div className="display">{props.result}</div>
+    </div>
+  );
 };
 
 const mapStateToProps = ({ calculatorState }) => {
-  return { result: calculatorState.display };
+  return {
+    result: calculatorState.display,
+    operator: calculatorState.operator
+  };
 };
 
 export default connect(mapStateToProps)(Display);
